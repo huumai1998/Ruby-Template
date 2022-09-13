b86d2ff8c5f6a81d558a836919db3443f391134b
huumai1998 <huumai0918@gmail.com>
2022-09-13 01:16:01 -0600

    UpdateLinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'home/index'
+  #change roots routes
+  get 'home/about'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
